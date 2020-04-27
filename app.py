b0f5719158a2eb331b601704ef91f444f7e77fdf
Snoudin <denis.r.vasiuk@gmail.com>
2020-04-27 22:30:05 +1000

    upd: threads
    TODO: convert

--- app.py
+++ app.py
@@ -1,18 +1,55 @@
 from sound import get_length, bad_sound
 from xlsxwriter import Workbook as wb
 from os.path import expanduser
+from threading import Thread
 
 
-def main():  # creating answer files, calling info functions
+class Test(Thread):
+    def __init__(self, name):
+        Thread.__init__(self)
+        self.name = name
+
+    def run1(self):  # 1st table
+        pass
+
+    def run2(self):  # 2nd table
+        pass
+
+    def run3(self):  # final table
+        pass
+
+
+def test1():
     level1 = wb(expanduser('~\\Desktop\\Критерии 1-го уровня.xlsx'))
-    level2 = wb(expanduser('~\\Desktop\\Критерии 2-го уровня.xlsx'))
-    last = wb(expanduser('~\\Desktop\\Крупности.xlsx'))
-
-    # magic
-
+    for i in range(8):
+        name = "Thread #%s" % i
+        my_thread = Test(name)
+        my_thread.run1(i)
     level1.close()
+
+
+def test2():
+    level2 = wb(expanduser('~\\Desktop\\Критерии 2-го уровня.xlsx'))
+    for i in range(8):
+        name = "Thread #%s" % i
+        my_thread = Test(name)
+        my_thread.run2(i)
     level2.close()
+
+
+def final_test():
+    last = wb(expanduser('~\\Desktop\\Крупности.xlsx'))
+    for i in range(8):
+        name = "Thread #%s" % i
+        my_thread = Test(name)
+        my_thread.run3(i)
     last.close()
+
+
+def main():
+    test1()
+    # test2()
+    # final_test()
 
 
 if __name__ == "__main__":
--- sound.py
+++ sound.py
@@ -1,19 +1,28 @@
 import wave
 from pydub import AudioSegment
-import subprocess
+from moviepy.editor import VideoFileClip
+from os import system
 
 
-def get_length(source):
-    wav = wave.open(source, mode="rb")
-    (nchannels, sampwidth, framerate, nframes, comptype, compname) = wav.getparams()
+def get_wav(source, thread):
+    VideoFileClip(source).audio.write_audiofile(f'{thread}.mp3')
+
+
+def get_length(source, thread):
+    get_wav(source, thread)
+    AudioSegment.from_mp3(f'{thread}.mp3').export(f'{thread}.wav', format="wav")
+    wav = wave.open(f'{thread}.wav', mode="rb")
+    nchannels, sampwidth, framerate, nframes, comptype, compname = wav.getparams()
     return nframes / framerate
 
 
-def bad_sound(source):
-    command = '''ffmpeg -i /test.avi -ab 160k -ac 2 -ar 44100 -vn audio.wav'''
-    subprocess.call(command, shell=True)
-    song = AudioSegment.from_wav(source)
+def bad_sound(source, thread):
+    get_wav(source, thread)
+    song = AudioSegment.from_mp3(f'{thread}.wav')
     for i in range(len(song) - 1):
         if abs(song[i] - song[i + 1]) > 15:
             return 'bad'
     return 'good'
+
+
+print(get_length('GH012651.MP4', 0))
