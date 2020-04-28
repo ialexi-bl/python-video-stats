52f8ff8dcd3bd67354c31f61786b489efc7817d0
Snoudin <denis.r.vasiuk@gmail.com>
2020-04-28 19:36:41 +1000

    frezzes somewhere need to modify sound.py

--- src/slideshow.py
+++ src/slideshow.py
@@ -4,17 +4,18 @@ import cv2
 def check_slideshow(path):
     video = cv2.VideoCapture(path)
     prev = [[[]]]
-    cnt = 0
+    cnt = 1
     fps = video.get(cv2.CAP_PROP_FPS)
     while video.isOpened():
         ret, frame = video.read()
         if not ret:
             break
-        if frame == prev:
+        if len(frame) == len(prev) and all([all([all([frame[i][j][k] == prev[i][j][k] for k in range(3)])]
+                                                for j in range(len(frame[0])))] for i in range(len(frame))):
             cnt += 1
         else:
             prev = frame
-            cnt = 0
+            cnt = 1
         if fps / cnt < 25:
             return 'да'
     return 'нет'
--- src/sound.py
+++ src/sound.py
@@ -6,7 +6,6 @@ from .config import TEMP_FOLDER
 def bad_sound(path: str, thread):
     temp_file = join(TEMP_FOLDER, f"{thread}.mp3")
 
-
     song = AudioSegment.from_mp3(temp_file)
     for i in range(len(song) - 1):
         if abs(song[i] - song[i + 1]) > 15:
