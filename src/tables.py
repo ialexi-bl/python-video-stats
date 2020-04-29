c65b86fb9a2ae1e94df03d7e90ed7a26fd6f59e9
Snoudin <denis.r.vasiuk@gmail.com>
2020-04-29 14:06:14 +1000

    more fixes for god of fixes

--- src/tables.py
+++ src/tables.py
@@ -5,9 +5,7 @@ from .stats import get_stats
 from .xlsx import Xlsx
 from .defects.blur import blur_check
 from .sound import bad_sound
-from .brightness import bad_brightness
 from .slideshow import check_slideshow
-from .defects.horizon import check_rotation_deffects
 
 results1 = []
 results2 = []
@@ -45,7 +43,7 @@ class FirstTableThread(Thread):
                     res["orientation"] = "В"
                 else:
                     res["orientation"] = "Г"
-                rot = check_rotation_deffects(video)
+                rot = [False, False]
                 res["rotated"] = "да" if rot[1] else "нет"
                 res["unstable"] = "да" if rot[0] else "нет"
                 res["unfocused"] = "нет" if blur_check(video) else "да"
